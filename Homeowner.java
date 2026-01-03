a3ccd56bab04e6eb666795c00416056d7574f30d
Sam Webb <sam@samwebb.me>
2026-01-03 20:48:40 +0000

    Create initial homeowner class

---
+++ Homeowner.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Homeowner extends User {
+    private List<Property> properties;
+
+    public Homeowner(String userId, String name, String email, String passHash, List<Property> properties) {
+        super(userId, name, email, passHash);
+        this.properties = properties;
+    }
+
+    public List<Property> viewProperties() {
+        return properties;
+    }
+}
