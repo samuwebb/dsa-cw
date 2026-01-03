463337030ac436687fe01ac3ec4c4376756c5b06
Sam Webb <sam@samwebb.me>
2026-01-03 18:48:23 +0000

    Create user class constructor

--- User.java
+++ User.java
@@ -1,8 +1,21 @@
 public class User {
+    private String userId;
+    private String name;
+    private String email;
+    private String passHash;
 
     public User(String userId, String name, String email, String passHash) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.passHash = passHash;
     }
 
-    public static void register() {
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
