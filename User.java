ff31144aceeae845f51a3f0f74e424004f3c3330
Sam Webb <sam@samwebb.me>
2026-01-03 18:56:21 +0000

    Change private variables to protected

--- User.java
+++ User.java
@@ -1,8 +1,8 @@
 public class User {
-    private String userId;
-    private String name;
-    private String email;
-    private String passHash;
+    protected String userId;
+    protected String name;
+    protected String email;
+    protected String passHash;
 
     public User(String userId, String name, String email, String passHash) {
         this.userId = userId;
