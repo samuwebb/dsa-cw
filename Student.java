acb6b2a44bbdfc6ada2d1cf7dc070f08f93c34e1
Sam Webb <sam@samwebb.me>
2026-01-03 18:56:34 +0000

    Create initial student class

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student extends User {
+    private String studentId;
+    private String university;
+
+    public Student(String userId, String name, String email, String passHash, String studentId, String university) {
+        super(userId, name, email, passHash);
+        this.studentId = studentId;
+        this.university = university;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+}
