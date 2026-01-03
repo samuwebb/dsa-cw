9b9b947b6b3d8ff202b141934cd323172989e7d0
Sam Webb <sam@samwebb.me>
2026-01-03 21:02:58 +0000

    Create initial review class

---
+++ Review.java
@@ -0,0 +1,11 @@
+public class Review {
+    Property property;
+    int rating;
+    String comment;
+
+    public Review(Property property, int rating, String comment) {
+        this.property = property;
+        this.rating = rating;
+        this.comment = comment;
+    }
+}
