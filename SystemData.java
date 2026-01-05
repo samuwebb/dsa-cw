c49c52341f9c90db6ca6ea82333c8f6ddeb97d79
Sam Webb <sam@samwebb.me>
2026-01-05 16:53:18 +0000

    Create initial system data class

---
+++ SystemData.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SystemData {
+    private List<Student> students = new ArrayList<>();
+    private List<Homeowner> homeowners = new ArrayList<>();
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public List<Homeowner> getHomeowners() {
+        return homeowners;
+    }
+}
