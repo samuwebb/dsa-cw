78e346833d6408a9d5904c6833e449fb3898f147
Sam Webb <sam@samwebb.me>
2026-01-05 16:55:56 +0000

    Add registration objects to system data

--- Registration.java
+++ Registration.java
@@ -2,16 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Registration {
+    SystemData systemData;
 
     public Homeowner registerHomeowner(String userId, String name, String email, String passHash,
             List<Property> properties) {
         Homeowner newOwner = new Homeowner(userId, name, email, passHash, properties);
+        systemData.getHomeowners().add(newOwner);
         return newOwner;
     }
 
     public Student registerStudent(String userId, String name, String email, String passHash, String studentId,
             String university) {
         Student newStudent = new Student(userId, name, email, passHash, studentId, university);
+        systemData.getStudents().add(newStudent);
         return newStudent;
     }
 }
