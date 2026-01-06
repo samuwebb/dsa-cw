0442de208b5d300d9e193b965c95013b044aac5a
Sam Webb <sam@samwebb.me>
2026-01-06 01:02:21 +0000

    Add room data

--- SystemData.java
+++ SystemData.java
@@ -5,6 +5,7 @@ public class SystemData {
     private List<Student> students = new ArrayList<>();
     private List<Homeowner> homeowners = new ArrayList<>();
     private List<Property> properties = new ArrayList<>();
+    private List<Room> rooms = new ArrayList<>();
 
     public List<Student> getStudents() {
         return students;
@@ -16,5 +17,9 @@ public class SystemData {
 
     public List<Property> getProperties() {
         return properties;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
     }
 }
