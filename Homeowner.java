74930df6c58fb749ad333a11f5ba8838d7d2b24c
Sam Webb <sam@samwebb.me>
2026-01-08 01:31:16 +0000

    Fix homeowner property list null

--- Homeowner.java
+++ Homeowner.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Homeowner extends User {
-    private List<Property> properties;
+    private List<Property> properties = new ArrayList<>();
 
     public Homeowner(String name, String email, String passHash, List<Property> properties) {
         super(name, email, passHash);
